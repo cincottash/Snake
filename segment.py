e5e8a3bb82c7d8fd7160845c3312e9f384010984
cincottash <cincotta@udel.edu>
2020-03-02 01:53:51 -0500

    Added growth

--- segment.py
+++ segment.py
@@ -2,5 +2,4 @@ class snakeSegment:
 	def __init__(self, posx, posy, size):
 		self.posx = posx
 		self.posy = posy
-
 		self.size = size
--- update.py
+++ update.py
@@ -44,12 +44,28 @@ def snakeUpdate():
 
 def appleUpdate():
 	if(len(appleLocations) == 0):
-		appleLocations.append(Apple(random.randint(11, resolution-11), random.randint(11, resolution-11), 10))
+		appleLocations.append(Apple(random.randint(0, resolution), random.randint(0, resolution), 10))
 	else:
+		print(snakeSegmentLocations[len(snakeSegmentLocations)-1].posx)
 		for apple in appleLocations:
 			distance = math.sqrt((apple.posx-snakeSegmentLocations[0].posx)**2 + (apple.posy-snakeSegmentLocations[0].posy)**2)
 			if(distance < 10):
 				appleLocations.remove(apple)
+				#Spawn it behind the last segment
+				previousPosx = snakeSegmentLocations[len(snakeSegmentLocations)-1].posx
+				previousPosy = snakeSegmentLocations[len(snakeSegmentLocations)-1].posy
+				
+				#Moving down
+				if(dx == 0 and dy == 0.25):
+					snakeSegmentLocations.append(snakeSegment(previousPosx, previousPosy-11, 10))
+				#Moving up
+				elif(dx == 0 and dy == -0.25):
+					snakeSegmentLocations.append(snakeSegment(previousPosx, previousPosy+11, 10))
+				#Moving left
+				elif(dx == -0.25 and dy == 0):
+					snakeSegmentLocations.append(snakeSegment(previousPosx+11, previousPosy, 10))
+				elif(dx == 0.25 and dy == 0):
+					snakeSegmentLocations.append(snakeSegment(previousPosx-11, previousPosy, 10))
 			else:
 				pygame.draw.rect(canvas, RED, (apple.posx, apple.posy, apple.size, apple.size))
 
