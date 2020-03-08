cfdaeccabc8d6194ee8066249f3f538638fa1c54
cincottash <cincotta@udel.edu>
2020-03-08 10:53:28 -0400

    Progress

--- globals.py
+++ globals.py
@@ -4,6 +4,7 @@ WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 
+
 delta = 10
 
 snakeSize = 10
--- update.py
+++ update.py
@@ -1,9 +1,11 @@
 from globals import *
 from setup import *
 import math
+global foundApple
 global dy
 global dx 
 
+foundApple = False
 dy = 0
 dx = 0
 
@@ -11,6 +13,7 @@ def snakeUpdate():
 	#print(len(snakeSegmentLocations))
 	global dy
 	global dx
+	global foundApple
 	canvas.fill(WHITE)
 
 	for event in pygame.event.get():	
@@ -41,21 +44,23 @@ def snakeUpdate():
 				exit(0)
 
 			#TODO: Add collision detection of head on apple.
-			for apple in appleLocation:
-				distance = math.sqrt((apple[0]-snakeSegmentLocations[0][0])**2 + (apple[1]-snakeSegmentLocations[0][1])**2)
-				if(distance < 10):
-					print("found apple")
+			if(foundApple):
+				print("foundApple")
+
 			pygame.draw.rect(canvas, BLACK, (snakeSegmentLocations[i][0], snakeSegmentLocations[i][1], 10, 10))
 
 
 def appleUpdate():
+	global foundApple
 	if(len(appleLocation) == 0):
 		appleLocation.append([random.randint(11, resolution-11), random.randint(11, resolution-11)])
+		foundApple = False
 	else:
 		for apple in appleLocation:
 			distance = math.sqrt((apple[0]-snakeSegmentLocations[0][0])**2 + (apple[1]-snakeSegmentLocations[0][1])**2)
 			if(distance < 10):
 				appleLocation.remove(apple)
+				foundApple = True
 			else:
 				pygame.draw.rect(canvas, RED, (apple[0], apple[1], 10, 10))
 
